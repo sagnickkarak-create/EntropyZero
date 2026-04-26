b5cb8d7ae45a2d245736aac2ea94dd9fc7e56467
SagnickKarak <sagnickkarak@gmail.com>
2026-04-26 18:53:33 +0530

    Added sorter_logic : progress - creates files from the rules table into the selected target folder

--- EntropyZero/Parent_Folder/sorter_logic.py
+++ EntropyZero/Parent_Folder/sorter_logic.py
@@ -7,13 +7,26 @@ class FolderBuilder:
         self.path = path
         self.tunnel = Tunnel()
     
-    def builder(self):
+    def info_getter(self):
         self.tunnel.fetch_rules()
         self.rules = self.tunnel.rules
-        for i in self.rules :
-            print(f"{i['EXTENSION']}  -->  {i['PATH']}")
+        self.folders = []
+        self.extensions = []
+        for i in self.rules:
+            self.folders.append(i['PATH'])
+            self.extensions.append(i['EXTENSION'])
+        
+    def builder(self):
+        os.chdir(path)
+        for i in self.folders:
+            os.makedirs(i)
+        print("DONE!")
+
+    def mover(self):
+        pass
 
 if __name__=='__main__':
-    path = r'C:\Users\skull\Downloads'
+    path = r'G:/TEST/' # TEST is a temporary folder that I made for testing
     FB = FolderBuilder(path)
+    FB.info_getter()
     FB.builder()
